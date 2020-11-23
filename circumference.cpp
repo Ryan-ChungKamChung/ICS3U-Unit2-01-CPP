355651dbaf4291bd7552fc41bc435d31dcf56df7
Ubuntu <ubuntu@ip-172-31-54-182.ec2.internal>
2020-11-23 16:24:57 +0000

    fixed with proper style

--- circumference.cpp
+++ circumference.cpp
@@ -8,7 +8,8 @@
 #include <cmath>
 
 int main() {
-    std::cout << "The circumference of a circle with a radius of 15mm is: " << std::endl;
+    std::cout << "The circumference of a circle"
+        "with a radius of 15mm is: " << std::endl;
     std::cout << 2*M_PI*15 << "mm" << std::endl;
     std::cout << "The area of a circle with a radius of 15mm is: " << std::endl;
     std::cout << (pow(15, 2)*M_PI) << "mm^2" << std::endl;
